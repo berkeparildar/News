9be689896dc8187b3337d19b92775a81aa915216
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2024-06-20 23:14:44 +0300

    Initial commit

---
+++ News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Berke Parıldar on 20.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Berke Parıldar on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
